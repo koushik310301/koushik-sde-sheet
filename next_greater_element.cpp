3dd435c4851f735f2f69716a25de2e1048549e7c
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2022-06-18 11:23:58 +0530

    find the next greater element for each element in an array

---
+++ next_greater_element.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+vector<int> nextGreater(vector<int> &arr, int n) {
+    // Write your code here
+    stack<int> st;
+    vector<int> ans(n,-1);
+    st.push(0);
+    for(int i=0;i<n;i++){
+        if(arr[i]<=arr[st.top()]) st.push(i);
+        else{
+            while(st.empty()==false && arr[i]>arr[st.top()]){
+                ans[st.top()]=arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+    }
+    return ans;
+}
