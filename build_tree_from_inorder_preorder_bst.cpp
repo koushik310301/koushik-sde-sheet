fb30fa5d70f1e1a72a5d577536064983f78f6c65
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2022-06-18 15:06:10 +0530

    build a tree form inorder of a bst

--- build_tree_from_inorder_preorder_bst.cpp
+++ build_tree_from_inorder_preorder_bst.cpp
@@ -26,31 +26,28 @@
     };
 
 *************************************************************/
-#include<bits/stdc++.h>
 
-TreeNode<int>* build(vector<int> &ino, int inoStart, int inoEnd, vector<int> &pro, int proStart, int proEnd, unordered_map<int,int>& mp){
-    
-    if(inoStart>inoEnd || proStart>proEnd) return NULL;
-    
-    TreeNode<int>* root= new TreeNode<int>(pro[proStart]);
-    
-    int ind=mp[root->data];
-    int noOfEleOnLeft = ind-inoStart;
-    
-    root->left=build(ino,inoStart,ind-1,pro,proStart+1,proStart+noOfEleOnLeft,mp);
-    root->right=build(ino,ind+1,inoEnd,pro,proStart+noOfEleOnLeft+1,proEnd,mp);
-    
-    return root;
-    
-}
-
-TreeNode<int>* preOrderTree(vector<int> &pro){
+TreeNode<int>* preOrderTree(vector<int> &preOrder){
     // Write your code here.
-    unordered_map<int,int> mp;
-    vector<int> ino=pro;
-    sort(ino.begin(),ino.end());
-    for(int i=0;i<ino.size();i++){
-        mp[ino[i]]=i;
+    TreeNode<int>* root=new TreeNode<int>(preOrder[0]);
+    for(int i=1;i<preOrder.size();i++){
+        TreeNode<int>* ptr=root;
+         TreeNode<int>* prev=NULL;
+        while(ptr!=NULL){
+            prev=ptr;
+            if(preOrder[i]>ptr->data){
+                ptr=ptr->right;
+            }
+            else{
+                ptr=ptr->left;
+            }
+        }
+        if(preOrder[i]>prev->data){
+            prev->right=new TreeNode<int>(preOrder[i]);
+        }
+        else{
+            prev->left=new TreeNode<int>(preOrder[i]);
+        }
     }
-    return build(ino,0,ino.size()-1,pro,0,pro.size()-1,mp);
+    return root;
 }
