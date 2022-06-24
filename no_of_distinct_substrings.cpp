ecfd4f6d6b78a88dfd28ad34d43c87f297235bcb
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2022-06-24 17:43:10 +0530

    count no. of distinct substrings

---
+++ no_of_distinct_substrings.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+struct Node{
+      Node* links[26];
+    
+    bool containsKey(char ch){
+        return links[ch-'a'];
+    }
+    
+    void put(char ch, Node* node){
+        links[ch-'a']=node;
+    }
+    
+    
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    
+};
+
+int distinctSubstring(string &word) {
+    //  Write your code here.
+    int cnt=0;
+    Node* root=new Node();
+    for(int i=0;i<word.size();i++){
+        Node* node=root;
+        for(int j=i;j<word.size();j++){
+            if(node->containsKey(word[j])==false){
+                cnt++;
+                node->put(word[j],new Node);
+            }
+            node=node->get(word[j]);
+        }
+    }
+    return cnt;
+}
