069df99945b2500ec4b4bba7db0b209adb221ec3
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2022-06-24 18:56:39 +0530

    maximum meetings

---
+++ maximum_meetings.cpp
@@ -0,0 +1,64 @@
+// #include<bits/stdc++.h>
+// bool cmp(pair<int,int>& a, pair<int,int>& b){
+//     if(a.second==b.second) return a.first<b.first;
+//     return a.second<b.second;
+// }
+// vector<int> maximumMeetings(vector<int> &start, vector<int> &end) {
+//     // Write your code here.
+//     int n=start.size();
+//     vector<int> ans;
+//     stack<pair<int,int>> st;
+//     vector<pair<int,int>> v;
+//     for(int i=0;i<n;i++){
+//         v.push_back({start[i],end[i]});
+//     }
+//     sort(v.begin(),v.end(),cmp);
+    
+//     st.push(v[0]);
+    
+//     for(int i=1;i<n;i++){
+//         if(v[i].first>st.top().second){
+//             st.push({start[i],end[i]});
+//         }
+//     }
+//     cout<<"-------------------------"<<endl;
+//     return ans;
+    
+// }
+
+#include<bits/stdc++.h>
+//creating a structure
+struct meeting{
+    int start;
+    int end;
+    int pos;
+};
+bool comparator(struct meeting m1,meeting m2){
+    if(m1.end<m2.end) return true;
+    else if(m1.end>m2.end) return false;
+    else if(m1.pos<m2.pos) return true;
+    else return false;
+}
+vector<int> maximumMeetings(vector<int> &s, vector<int> &e) {
+    // Write your code here.
+    int n=s.size();
+    struct meeting meet[n];
+    for(int i=0;i<n;i++){
+        meet[i].start=s[i];
+        meet[i].end=e[i];
+        meet[i].pos=i+1;
+    }
+    sort(meet,meet+n,comparator);
+    vector<int> ans;
+    int ending=meet[0].end;
+    
+    ans.push_back(meet[0].pos);
+    
+    for(int i=1;i<n;i++){
+        if(meet[i].start>ending){
+            ending=meet[i].end;
+            ans.push_back(meet[i].pos);
+        }
+    }
+    return ans;
+}
