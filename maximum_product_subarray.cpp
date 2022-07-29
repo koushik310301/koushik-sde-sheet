495758eb4d2338b8009faa658ee50e9f70ee75d7
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2022-07-29 23:54:00 +0530

    maximum product subarrays

---
+++ maximum_product_subarray.cpp
@@ -0,0 +1,18 @@
+int maximumProduct(vector<int> &nums, int n)
+{
+	// Write your code here
+    int ans=nums[0];
+        int ma=ans;
+        int mi=ans;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<0){
+                swap(ma,mi);
+            }
+            ma=max(nums[i],nums[i]*ma);
+            mi=min(nums[i],nums[i]*mi);
+            ans=max(ans,ma);
+        }
+        
+        
+        return ans;
+}
